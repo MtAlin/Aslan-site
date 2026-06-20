022e962c5a3c3158f0fd2399f5abd4071dc30f8d
Alin Matusa <106775017+MtAlin@users.noreply.github.com>
2026-06-20 14:18:45 +0300

    Fix Vercel SSR

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import vercel from "nitropack/presets/vercel";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  nitro: {
-    preset: vercel(),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
   },
-});
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
