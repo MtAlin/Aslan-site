1dcc1f3dc6e9948640892e3b6ef00a07d770b811
Alin Matusa <106775017+MtAlin@users.noreply.github.com>
2026-06-20 13:27:39 +0300

    Add an logo resize

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           <img
             src={Logo}
             alt="Biblioteca Aslan Logo"
-            className="w-[110px] h-[60px] object-contain transition-transform group-hover:scale-110"
+            className="w-[110px] h-[50px] object-contain transition-transform group-hover:scale-110"
           />
           {/* <span className="font-display font-bold text-xl text-foreground">
             Biblioteca <span className="text-warm-gold">Aslan</span>
